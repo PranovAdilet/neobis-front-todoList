34b3e83a26fcafb1f6aeba6ae0b1f004cf9518d3
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-01-03 09:19:10 +0300

    commit

---
+++ tasks/tasks.js
@@ -0,0 +1,241 @@
+//Part 1
+
+//1
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function() {
+//         console.log(i);
+//     })
+// };
+// В консоли выведутся только десятки, потому что происходит замыкание, а так же из-за использования var
+
+//2
+
+// const currentDateFunction = () => {
+//     const daysInWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//
+//
+//     const currentDate = new Date()
+//
+//
+//     let currentHours = currentDate.getHours()
+//     let currentMinutes = currentDate.getMinutes()
+//     let currentSeconds = currentDate.getSeconds()
+//
+//     const formatHours = currentHours % 12 || 12
+//
+//     const formatMinutes = currentMinutes < 10 ? '0' + currentMinutes : currentMinutes
+//
+//     const formatSeconds = currentSeconds < 10 ? '0' + currentSeconds : currentSeconds
+//
+//     const ampm = currentHours >= 12 ? 'PM' : 'AM'
+//
+//
+//     return `Today is: ${daysInWeek[currentDate.getDay()]}.
+// Current time is: ${formatHours}${ampm}:${formatMinutes}:${formatSeconds}`
+// }
+//
+// console.log(currentDateFunction())
+
+//3
+
+// const reverseFunction = (n) => {
+//     return +n.toString().split("").reverse().join("")
+// }
+//
+// console.log(reverseFunction(234621))
+
+//4
+
+// const factorialOfNum = (num) => {
+//     let result = 1
+//
+//     for (let i = 1; i <= num; i++) {
+//         result *= i
+//     }
+//     return `${num}! = ${result}`
+// }
+//
+// console.log(factorialOfNum(5))
+
+//5
+
+// const promtFunction = () => {
+//     let num1 = prompt("Введите свое первое число")
+//     let num2 = prompt("Введите свое второе число")
+//
+//     if (!isNaN(+num1) && !isNaN(+num2)){
+//         return +num1 > +num2 ? alert(num1) : alert(num2)
+//     }else{
+//         alert("Данные которые вы ввели не являются числами!")
+//     }
+//
+// }
+// promtFunction()
+
+//6
+
+// const colorsToString = (arr) => {
+//     return arr.join(",")
+// }
+// console.log(colorsToString(["Red", "Green", "White", "Black"]))
+
+//7
+
+// const monthName = (date) => {
+//
+//     const months = ["January","February","March","April","May","June","July","August","September", "October", "November", "December"]
+//
+//     let month = date.getMonth()
+//
+//     return months[month]
+//
+// }
+// console.log(monthName(new Date("02/06/2009")))
+// console.log(monthName(new Date("11/13/2009")))
+
+//8
+
+// const isUppercase = (string) => {
+//     let firstChar = string.slice(0,1)
+//     return firstChar === firstChar.toUpperCase()
+// }
+// console.log(isUppercase('Hello'))
+
+//9
+
+// const smileFunction = () => {
+//     console.log(":)");
+//
+//     console.log("   ******   ");
+//     console.log(" *        * ");
+//     console.log("*   O    O   *");
+//     console.log("*     ||     *");
+//     console.log(" * \\____/ * ");
+//     console.log("  *      *  ");
+//     console.log("   ******   ");
+// }
+// smileFunction()
+
+
+
+
+
+
+
+
+
+//  Part 2
+
+//1
+// const patterFunction = () => {
+//     let arr = [3, 5, 2, 4, 6, 1]
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(document.querySelector(`#string-${arr[i]}`).textContent)
+//     }
+// }
+//
+// patterFunction()
+
+//2
+
+// const elementsColor = () => {
+//     let elems = document.querySelectorAll('.element')
+//
+//     return  elems.forEach((item, idx) => {
+//         if (idx < 3){
+//             item.style.color = `red`
+//         }else {
+//             item.style.color = "green"
+//         }
+//     })
+// }
+//
+// elementsColor()
+
+//3
+
+// const tasks = ['Buy lemonade', 'Make toasts', 'Repair car', 'Play games', 'Pet a cat'];
+//
+// const addElements = () => {
+//     let todoList = document.querySelector("#todo-list")
+//
+//     tasks.forEach((item) => {
+//         let newElement = document.createElement("li")
+//         newElement.className = "task"
+//         newElement.textContent = item
+//         todoList.appendChild(newElement)
+//     })
+// }
+// addElements()
+
+//4
+
+// const addHrFunction = () => {
+//     let pElements = document.querySelectorAll('p')
+//
+//
+//     pElements.forEach(p => {
+//         let hr = document.createElement("hr")
+//         p.append(hr)
+//     })
+//
+//     console.log(pElements)
+// }
+//
+// addHrFunction()
+
+//5
+
+// const cartFunction = () => {
+//     const products = document.querySelectorAll('.item')
+//     const parent = document.querySelector('#cart-items')
+//
+//     products.forEach(product => {
+//         if (product.textContent === 'Cola 1.5 l. x 1'){
+//             product.remove()
+//         }
+//         if (product.textContent === 'Chocolate bar x 3'){
+//             let span = document.createElement('span')
+//             let newProduct = document.createElement("div")
+//
+//             newProduct.textContent = 'Canned Fish '
+//             span.textContent = 'x 4'
+//
+//             newProduct.appendChild(span)
+//
+//            parent.replaceChild(newProduct, product )
+//         }
+//     })
+// }
+//
+// cartFunction()
+
+//6
+
+// const createList = () => {
+//
+//     let body = document.querySelector('body')
+//
+//     let list = document.createElement('ul')
+//
+//
+//     let count = 1
+//     let promptFunction = (c) => {
+//         return prompt(`Введите содержимое ${c} пункта`)
+//     }
+//     while (true) {
+//         let userInput = promptFunction(count)
+//         if (!userInput) {
+//             break
+//         }
+//         let li = document.createElement("li")
+//         li.textContent = userInput
+//         list.appendChild(li)
+//         count++
+//     }
+//     body.appendChild(list)
+//
+// }
+//
+// createList()
---
+++ todo.js
@@ -0,0 +1,136 @@
+const todoForm = () => {
+    let inputName = document.querySelector('.todo__title-input')
+
+    let inputTask = document.querySelector('.todo__input')
+
+    let icons = document.querySelectorAll('.icon')
+
+    let btn = document.querySelector('.todo__btn')
+
+    let classNameIcon = ''
+
+    const selectCategory = (clickedIcon) => {
+        classNameIcon = clickedIcon.classList.contains('icon__business') ? 'icon__business' : 'icon__personal'
+        icons.forEach(icon => {
+            if (icon === clickedIcon) {
+                icon.classList.toggle('icon-circle-empty')
+                icon.classList.toggle('icon-dot-circled')
+            } else {
+                icon.classList.add('icon-circle-empty')
+                icon.classList.remove('icon-dot-circled')
+            }
+        })
+    }
+
+    icons.forEach(icon => icon.addEventListener('click', () => selectCategory(icon)))
+
+    const editTodo = (e, text, input, editBtn) => {
+
+        if (e.target.textContent === "Edit") {
+            input.value = text.textContent
+            text.style.display = 'none'
+            input.style.display = 'block'
+
+            input.focus()
+            input.selectionStart = input.selectionEnd = input.value.length
+
+            editBtn.innerHTML = "Save"
+        } else if (e.target.textContent === "Save") {
+            text.textContent = input.value
+            text.style.display = 'block'
+            input.style.display = 'none'
+            editBtn.innerHTML = "Edit"
+        }
+        input.addEventListener('blur', () => {
+            text.textContent = input.value;
+            text.style.display = 'block';
+            input.style.display = 'none';
+            editBtn.innerHTML = "Edit";
+        });
+    }
+
+    const deleteTodo = (parent, newTask) =>  {
+        parent.removeChild(newTask)
+    }
+
+    const checkTasks = (e, newTask, text, input) => {
+
+        if (e.target.classList.contains('icon-circle-empty')){
+            e.target.classList.toggle('icon-circle-empty')
+            e.target.classList.toggle('icon-dot-circled')
+
+            text.style.textDecoration = 'line-through'
+            text.style.color = 'grey'
+            input.style.color = 'grey'
+            input.style.textDecoration = 'line-through'
+
+        }else {
+            e.target.classList.add('icon-circle-empty')
+            e.target.classList.remove('icon-dot-circled')
+            e.target.classList.remove('line-through')
+
+            text.style.textDecoration = 'none'
+            input.style.textDecoration = 'none'
+
+            text.style.color = '#302f3d'
+            input.style.color = '#302f3d'
+        }
+    }
+
+    const addTodo = () => {
+
+        let parent = document.querySelector('.todo__list')
+        let newTask = document.createElement("div")
+
+        if (inputTask.value.length < 1){
+           alert("You need to write something!")
+            return
+        }else if (classNameIcon.length < 1){
+            alert("Select a category!")
+            return
+        }else {
+            newTask.innerHTML = `<div class="todo__item">
+                <div class="todo__item-left">
+                    <i class="icon-circle-empty todo__item-icon ${classNameIcon}"></i>
+                    <p class="todo__item-text">${inputTask.value}</p>
+                    <input class="todo__item-input" value="${inputTask.value}"/>
+                </div>
+                <div class="todo__item-btns">
+                    <button class="todo__btn-edit">Edit</button>
+                    <button class="todo__btn-delete">Delete</button>
+                </div>
+            </div>`
+
+            parent.appendChild(newTask)
+
+            inputTask.value = ''
+            classNameIcon = ''
+
+            icons.forEach(icon => {
+                icon.classList.add('icon-circle-empty')
+                icon.classList.remove('icon-dot-circled')
+            })
+        }
+
+        const deleteBtn = newTask.querySelector('.todo__btn-delete')
+        const editBtn = newTask.querySelector('.todo__btn-edit')
+
+        let text = newTask.querySelector('.todo__item-text')
+        let input = newTask.querySelector('.todo__item-input')
+        let checkBtn = newTask.querySelector('.todo__item-icon')
+
+        editBtn.addEventListener('click', (e) => editTodo(e, text, input, editBtn))
+        deleteBtn.addEventListener('click', () => deleteTodo(parent, newTask))
+        checkBtn.addEventListener('click', (e) => checkTasks(e, newTask, text, input))
+
+    }
+
+
+    btn.addEventListener('click', addTodo)
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    todoForm();
+})
